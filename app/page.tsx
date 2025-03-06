8ae1a9bbfbba11c8711c27ca35b3f8f68a9c75ca
Polqt <poyhidalgo@gmail.com>
2025-03-06 20:44:40 +0800

    feat: Implement authentication pages and layouts; enhance login form with validation and toast notifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
+import { BackgroundBeams } from '@/components/ui/background-beams';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <div className="w-100 min-h-screen bg-slate-900">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className=""></h1>
+    <div className="min-h-screen relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4 text-center">
+        <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 font-sans font-bold mb-4">
+          Welcome to BudgetBuddy
+        </h1>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm relative z-10 mb-8">
+          Take control of your finances with my easy-to-use budget tracker.{' '}
+          <br />
+          Track your income, expenses, and savings all in one place.
+        </p>
+        <Button
+          variant={'outline'}
+          size={'lg'}
+          asChild
+          className="relative z-10"
+        >
+          <Link href="/login">Get Started</Link>
+        </Button>
+        <BackgroundBeams />
       </div>
-=======
-    <div>
-      
->>>>>>> 673936538959f34e43dc7ce4e4a568db096043ec
     </div>
   );
 }
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -3,12 +3,23 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Form, FormControl, FormField, FormItem, FormLabel } from './ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
+import Link from 'next/link';
+import { toast } from 'sonner';
 
 const formSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email({
+    message: 'Please enter a valid email address.',
+  }),
   password: z.string().min(8, {
     message: 'Password must be at least 8 characters long.',
   }),
@@ -18,7 +29,7 @@ export default function LoginForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: 'poyhidalgo@example.com',
+      email: '',
       password: '',
     },
   });
@@ -28,22 +39,56 @@ export default function LoginForm() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name='email'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="poyhidalgo@example.com" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button type='submit'>Login</Button>
-      </form>
-    </Form>
+    <div className="border py-8 px-6 rounded-lg shadow-md w-full max-w-md mx-auto">
+      <h2 className="text-2xl font-bold mb-6 text-center">Welcome Back</h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="poyhidalgo@example.com"
+                    className="w-full"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <Input id="password" type="password" {...field} />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="w-full mt-6"
+            onClick={() => {
+              toast.success('Login successful')
+            }}
+          >
+            Login
+          </Button>
+        </form>
+      </Form>
+      <div className="text-center text-slate-900/50 text-sm mt-6">
+        Don&apos;t have an account?{' '}
+        <Link href={'/register'} className="hover:text-primary font-medium">
+          Register now
+        </Link>
+      </div>
+    </div>
   );
 }
